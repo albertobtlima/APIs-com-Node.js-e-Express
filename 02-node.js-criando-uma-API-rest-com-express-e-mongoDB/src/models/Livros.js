ab48238af05a39a548e0fef83a339092d508d502
Alberto Lima <alberto.btlima@outlook.com>
2025-03-05 18:27:40 -0300

    add Livros.js

---
+++ 02-node.js-criando-uma-API-rest-com-express-e-mongoDB/src/models/Livros.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const livrosSchema = new mongoose.Schema(
+  {
+    id: { type: mongoose.Schema.Types.ObjectId },
+    titulo: { type: String, required: true },
+    editora: { type: String, required: true },
+    preco: { type: Number },
+    paginas: { type: Number },
+  },
+  { versionKey: false }
+);
+
+const livro = mongoose.model("livros", livrosSchema);
+
+export default livro;
