868a8f3da160500df7740cd5337c96875f3b4c8b
Alberto Lima <alberto.btlima@outlook.com>
2025-03-05 19:50:17 -0300

    add metodo atualizarLivro

--- 02-node.js-criando-uma-API-rest-com-express-e-mongoDB/src/controllers/livroController.js
+++ 02-node.js-criando-uma-API-rest-com-express-e-mongoDB/src/controllers/livroController.js
@@ -2,8 +2,26 @@ import livro from "../models/Livros.js";
 
 class LivroController {
   static async listarLivros(req, res) {
-    const listaLivros = await livro.find({});
-    res.status(200).json(listaLivros);
+    try {
+      const listaLivros = await livro.find({});
+      res.status(200).json(listaLivros);
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: `${error.message} - falha na requisição` });
+    }
+  }
+
+  static async listarLivroPorId(req, res) {
+    try {
+      const id = req.params.id;
+      const livroEncontrado = await livro.findById(id);
+      res.status(200).json(livroEncontrado);
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: `${error.message} - falha na requisição do livro` });
+    }
   }
 
   static async cadastrarLivro(req, res) {
@@ -14,6 +32,18 @@ class LivroController {
       res
         .status(500)
         .json({ message: `${error.message} - falha ao cadastrar livro` });
+    }
+  }
+
+  static async atualizarLivro(req, res) {
+    try {
+      const id = req.params.id;
+      await livro.findByIdAndUpdate(id, req.body);
+      res.status(200).json({ message: "Livro atualizado" });
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: `${error.message} - falha na atualização do livro` });
     }
   }
 }
--- 02-node.js-criando-uma-API-rest-com-express-e-mongoDB/src/routes/livrosRoutes.js
+++ 02-node.js-criando-uma-API-rest-com-express-e-mongoDB/src/routes/livrosRoutes.js
@@ -4,3 +4,11 @@ import LivroController from "../controllers/livroController.js";
 const routes = express.Router();
 
 routes.get("/livros", LivroController.listarLivros);
+
+routes.get("/livros/:id", LivroController.listarLivroPorId);
+
+routes.post("/livros", LivroController.cadastrarLivro);
+
+routes.put("/livros/:id", LivroController.atualizarLivro);
+
+export default routes;
