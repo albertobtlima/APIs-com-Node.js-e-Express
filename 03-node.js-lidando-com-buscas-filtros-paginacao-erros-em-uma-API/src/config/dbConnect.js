1ffd63a8dd2bfef691b4de0e38b25641c5e0eb3b
Alberto Lima <alberto.btlima@outlook.com>
2025-03-06 16:35:45 -0300

    fix conexao

--- 03-node.js-lidando-com-buscas-filtros-paginacao-erros-em-uma-API/src/config/dbConnect.js
+++ 03-node.js-lidando-com-buscas-filtros-paginacao-erros-em-uma-API/src/config/dbConnect.js
@@ -1,7 +1,9 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
-mongoose.connect("mongodb+srv://alura:123@alura.dkjed.mongodb.net/alura-node");
+mongoose.connect(process.env.STRING_CONEXAO_DB);
 
 let db = mongoose.connection;
 
 export default db;
+
+// // ?retryWrites=true&w=majority&appName=Cluster0
