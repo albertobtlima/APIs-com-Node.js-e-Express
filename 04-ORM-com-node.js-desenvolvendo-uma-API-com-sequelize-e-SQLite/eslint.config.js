523cec4e105d6d035b58bcb862f5d4a9d1fda108
Alberto Lima <alberto.btlima@outlook.com>
2025-03-11 20:36:30 -0300

    configuração sequelizer

---
+++ 04-ORM-com-node.js-desenvolvendo-uma-API-com-sequelize-e-SQLite/eslint.config.js
@@ -0,0 +1,8 @@
+const globals = require("globals");
+const pluginJs = require("@eslint/js");
+
+/** @type {import('eslint').Linter.Config[]} */
+module.exports = [
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+];
