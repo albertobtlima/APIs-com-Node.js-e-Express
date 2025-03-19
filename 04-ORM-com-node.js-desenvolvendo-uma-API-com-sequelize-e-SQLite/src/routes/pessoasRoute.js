59aee5da2166b2ab6f642d0f496a4350c22cbfe6
Alberto Lima <alberto.btlima@outlook.com>
2025-03-19 15:55:52 -0300

    add rotas, post, put, delete

--- 04-ORM-com-node.js-desenvolvendo-uma-API-com-sequelize-e-SQLite/src/routes/pessoasRoute.js
+++ 04-ORM-com-node.js-desenvolvendo-uma-API-com-sequelize-e-SQLite/src/routes/pessoasRoute.js
@@ -6,5 +6,9 @@ const pessoaController = new PessoaController();
 const router = Router();
 
 router.get("/pessoas", (req, res) => pessoaController.pegaTodos(req, res));
+router.get("/pessoa/:id", (req, res) => pessoaController.pegaUmPorId(req, res));
+router.post("/pessoa", (req, res) => pessoaController.criaNovo(req, res));
+router.put("/pessoa/:id", (req, res) => pessoaController.atualiza(req, res));
+router.delete("/pessoa/:id", (req, res) => pessoaController.exclui(req, res));
 
 module.exports = router;
