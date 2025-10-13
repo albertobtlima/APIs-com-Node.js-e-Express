b905f2fe86e630a44cc25422339b8a18529bdbdc
Alberto Lima <alberto.btlima@outlook.com>
2025-10-13 12:57:37 -0300

    add: pegaMatriculasPorEstudante

--- 05-ORM-avancando-nas-funcionalidades-do-sequelize/src/routes/pessoasRoute.js
+++ 05-ORM-avancando-nas-funcionalidades-do-sequelize/src/routes/pessoasRoute.js
@@ -31,6 +31,14 @@ router.get("/pessoas/:estudante_id/matriculas/todos", (req, res) =>
   pessoaController.pegaTodasAsMatriculas(req, res)
 );
 
+router.get("/pessoas/:estudante_id/matriculas/confirmadas", (req, res) =>
+  matriculaController.pegaMatriculasPorEstudante(req, res)
+);
+
+router.get("/pessoas/matriculas/lotadas", (req, res) =>
+  matriculaController.pegaCursosLotados(req, res)
+);
+
 router.get("/pessoas/:estudante_id/matriculas/:id", (req, res) =>
   matriculaController.pegaUm(req, res)
 );
